cc82dd4cd0d706dfa0dfbfe3f6f6ecfb62740578
ncmprbl <ncmprbll@gmail.com>
2024-08-01 20:36:04 +0700

    Base setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <p>
+        -- placeholder --
+      </p>
+    </main>
+  );
+}
---
+++ src/app/secret/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <main>
+      <p>
+        -- placeholder --
+      </p>
+    </main>
+  );
+}
+  
